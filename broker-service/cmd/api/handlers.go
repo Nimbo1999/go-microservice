e759fc091b00abc0bfafa83b80edc1d742d2b1eb
Matheus Lopes <matlopes1999@gmail.com>
2023-11-26 21:07:13 -0300

    add test request call to the frontend, also add helper functions to the backend code

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -1,15 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
-
-type jsonResponse struct {
-	Error   bool   `json:"error"`
-	Message string `json:"message"`
-	Data    any    `json:"data,omitempty"`
-}
 
 func (app *Config) Broker(response http.ResponseWriter, request *http.Request) {
 	payload := jsonResponse{
@@ -17,8 +10,5 @@ func (app *Config) Broker(response http.ResponseWriter, request *http.Request) {
 		Message: "Hello world!",
 	}
 
-	out, _ := json.MarshalIndent(payload, "", "\t")
-	response.Header().Add("Content-Type", "application/json")
-	response.WriteHeader(http.StatusOK)
-	response.Write(out)
+	app.writeJSON(response, http.StatusOK, payload)
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,46 @@
+import { useState } from "react";
 import SectionContent from "./components/section-content"
 
 function App() {
+  const [payloads, setPayloads] = useState<any[]>([]);
+  const [responses, setResponses] = useState<any[]>([]);
+
+  const format = (data: any) => JSON.stringify(data, undefined, 4);
+
   return (
     <main className="container">
       <header className="header">
         <h1>Test microservices</h1>
         <hr className="container__row-line" />
+        <button onClick={async () => {
+          setPayloads((prev) => [...prev, null])
+          try {
+            const request = await fetch('http://localhost:8080', { method: 'POST' });
+            const response = await request.json()
+            setResponses((prev) => [...prev, response])
+          } catch(err) {
+            console.error(err)
+          }
+        }}>
+          Test
+        </button>
       </header>
 
       <section className="container__content">
         <SectionContent title="Sent">
-          Nothing sent yet...
+          <div className="container__content-wrapper">
+              {payloads.length < 1 ? "Nothing sent yet..." : payloads.map(payload => (
+              <pre className="container__content-item">{format(payload)}</pre>
+              ))}
+          </div>
         </SectionContent>
 
         <SectionContent title="Received">
-          Nothing received yet...
+          <div className="container__content-wrapper">
+            {responses.length < 1 ? "Nothing sent yet..." : responses.map(response => (
+              <pre className="container__content-item">{format(response)}</pre>
+              ))}
+          </div>
         </SectionContent>
       </section>
 

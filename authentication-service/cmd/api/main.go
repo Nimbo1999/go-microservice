770b310cc50a10efbb9aa8c4f8797e35674afae3
Matheus Lopes <matlopes1999@gmail.com>
2024-01-06 22:32:33 -0300

    creates the authentication-service dockerfile and add it to docker compose

--- authentication-service/cmd/api/main.go
+++ authentication-service/cmd/api/main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"authentication/data"
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
 const webPort = "80"
@@ -19,9 +26,16 @@ func main() {
 	fmt.Println("Starting the authentication service!")
 
 	// TODO: connect to DB
+	conn, err := openDB(os.Getenv("DB_DNS"))
+	if err != nil {
+		log.Panicln(err)
+	}
 
 	// set up config
-	config := Config{}
+	config := Config{
+		DB:     conn,
+		Models: data.New(conn),
+	}
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%s", webPort),
 		Handler: config.routes(),
@@ -30,4 +44,18 @@ func main() {
 	if err := srv.ListenAndServe(); err != nil {
 		log.Panic(err)
 	}
+}
+
+func openDB(dns string) (*sql.DB, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	db, err := sql.Open("pgx", dns)
+	if err != nil {
+		return nil, err
+	}
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return db, err
 }
